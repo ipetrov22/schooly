197cdb9cf63b1335d4f0a025b8d3fb42454831c3
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-01 17:12:05 +0200

    add config file to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,9 @@
+const config = require('./config/config');
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-app.listen(3333, console.log(`Server is running on port ${3000}.`))
+app.listen(config.port, console.log(`Server is running on port ${config.port}.`));
