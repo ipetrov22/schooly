4751b28edb34188ef329ef40bc0b03f4314d6681
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-01 15:05:22 +0200

    create PostProblem component

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -3,16 +3,18 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Details from './components/Details';
+import PostProblem from './components/PostProblem/PostProblem';
 
 
 export default function AppRouter() {
     return (
         <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
 
-            <Route path="/details" component={Details}/>
+            <Route path="/details" component={Details} />
+            <Route path="/ask" component={PostProblem} />
 
             <Redirect to="/" />
         </Switch>
---
+++ client/src/components/PostProblem/PostProblem.js
@@ -0,0 +1,134 @@
+import SchoolIcon from '@material-ui/icons/School';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Button,
+    CssBaseline,
+    TextField,
+    Typography,
+    Container,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select
+} from '@material-ui/core';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    }
+}));
+
+export default function PostProblem() {
+    const classes = useStyles();
+    const [subject, setSubject] = useState('');
+    const [grade, setGrade] = useState('');
+
+    const changeSubject = (e) => {
+        setSubject(e.target.value);
+    }
+
+    const changeGrade = (e) => {
+        setGrade(e.target.value);
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <SchoolIcon style={{ fontSize: 50 }}>
+                    <LockOutlinedIcon />
+                </SchoolIcon>
+                <Typography component="h1" variant="h5">
+                    Задай въпрос
+                </Typography>
+
+                <form className={classes.form} noValidate>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Тема"
+                        name="topic"
+                        autoComplete="topic"
+                    />
+
+                    <FormControl className={classes.formControl}
+                        margin="normal"
+                        fullWidth
+                        required
+                    >
+                        <InputLabel>Предмет</InputLabel>
+                        <Select name="subject" value={subject} onChange={changeSubject}>
+                            <MenuItem value={"math"}>Математика</MenuItem>
+                            <MenuItem value={"bulgarian"}>Български език и литература</MenuItem>
+                            <MenuItem value={"english"}>Английски език</MenuItem>
+                            <MenuItem value={"german"}>Немски език</MenuItem>
+                            <MenuItem value={"french"}>Френски език</MenuItem>
+                            <MenuItem value={"russian"}>Руски език</MenuItem>
+                            <MenuItem value={"history"}>История и цивилизация</MenuItem>
+                            <MenuItem value={"geography"}>География и икономика</MenuItem>
+                            <MenuItem value={"biology"}>Биология и ЗО</MenuItem>
+                            <MenuItem value={"chemistry"}>Химия и ООС</MenuItem>
+                            <MenuItem value={"physics"}>Физика и астрономия</MenuItem>
+                            <MenuItem value={"informatics"}>Информатика и информационни технологии</MenuItem>
+                            <MenuItem value={"sports"}>Физическо възпитание и спорт</MenuItem>
+                            <MenuItem value={"music"}>Музика</MenuItem>
+                            <MenuItem value={"art"}>Изобразително изкуство</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    <FormControl className={classes.formControl}
+                        margin="normal"
+                        fullWidth
+                        required
+                    >
+                        <InputLabel>Клас</InputLabel>
+                        <Select name="grade" value={grade} onChange={changeGrade}>
+                            <MenuItem value={12}>12</MenuItem>
+                            <MenuItem value={11}>11</MenuItem>
+                            <MenuItem value={10}>10</MenuItem>
+                            <MenuItem value={9}>9</MenuItem>
+                            <MenuItem value={8}>8</MenuItem>
+                            <MenuItem value={7}>7</MenuItem>
+                            <MenuItem value={6}>6</MenuItem>
+                            <MenuItem value={5}>5</MenuItem>
+                            <MenuItem value={4}>4</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={1}>1</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    <TextField required
+                        name="description"
+                        margin="normal"
+                        label="Описание"
+                        fullWidth
+                        multiline />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Публикувай
+                        </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
