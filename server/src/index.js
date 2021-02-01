af0270b303412843388659a7243e55d987a90899
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-01 17:15:17 +0200

    server/add routes file

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const config = require('./config/config');
+const routes = require('./routes');
 
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(routes);
 
 app.listen(config.port, console.log(`Server is running on port ${config.port}.`));
