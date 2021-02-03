842a42e0aaf3111a17c9910081cafa812cde3da6
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-03 22:06:10 +0200

    client/create menu drawer component

---
+++ client/src/components/Header/Drawer/Drawer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
+export default function MenuDrawer() {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setOpen(open);
+    };
+
+    const list = (
+        <div
+            className={clsx(classes.list)}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                <ListItem button key={'profile'}>
+                    <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+                    <ListItemText primary={'Профил'} />
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem button key={'logout'}>
+                    <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+                    <ListItemText primary={'Излез'} />
+                </ListItem>
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            <React.Fragment key={'right'}>
+                <Button color="inherit" onClick={toggleDrawer(true)}><MenuIcon /></Button>
+                <Drawer anchor={'right'} open={open} onClose={toggleDrawer(false)}>
+                    {list}
+                </Drawer>
+            </React.Fragment>
+        </div>
+    );
+}
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
+import Drawer from './Drawer';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/Auth';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -19,6 +20,8 @@ const useStyles = makeStyles(() => ({
 
 export default function Header() {
     const classes = useStyles();
+    const auth = useContext(AuthContext);
+    console.log(auth);
 
     return (
         <div className={classes.root}>
@@ -29,6 +32,7 @@ export default function Header() {
                     </Typography>
                     <Button color="inherit" component={Link} to="/login">Влез</Button>
                     <Button color="inherit" component={Link} to="/register">Регистрирай се</Button>
+                    <Drawer/>
                 </Toolbar>
             </AppBar>
         </div>
