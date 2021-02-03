16a1a2437d5d0987a308fc2ec6b4a363297b86e4
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-03 22:13:49 +0200

    client/add logout function to userService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -7,3 +7,7 @@ export const register = (email, password) => {
 export const login = (email, password) => {
     return firebase.auth().signInWithEmailAndPassword(email, password);
 }
+
+export const logout = () => {
+    return firebase.auth().signOut();
+}
