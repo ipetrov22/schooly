083693542b67213dce0b70a5cfe1c3be492be1f7
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-03 13:28:41 +0200

    client/create firebase.js file

---
+++ client/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2YqFrE7cYnSQX0HA02OF_nWl_kht8gsc",
+    authDomain: "schooly-73851.firebaseapp.com",
+    projectId: "schooly-73851",
+    storageBucket: "schooly-73851.appspot.com",
+    messagingSenderId: "214886662464",
+    appId: "1:214886662464:web:1481ec0a154fce68c4faa5"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
