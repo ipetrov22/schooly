b23f4dfcca920efef4a63b6aab529ec17f5404f1
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-03 21:19:05 +0200

    client/create login function in userService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,3 +3,7 @@ import firebase from '../firebase';
 export const register = (email, password) => {
     return firebase.auth().createUserWithEmailAndPassword(email, password);
 }
+
+export const login = (email, password) => {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+}
