75595db567151a3b819e8c7cf0841c5f8e8822fb
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-05 23:27:39 +0200

    client/register and login pages redirect to '/' if user is auth

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -4,14 +4,21 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Details from './components/Details';
 import PostProblem from './components/PostProblem/PostProblem';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/Auth';
 
 
 export default function AppRouter() {
+    const auth = useContext(AuthContext);
     return (
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
+            <Route path="/login">
+                {auth ? <Redirect to="/" /> : <Login />}
+            </Route>
+            <Route path="/register">
+                {auth ? <Redirect to="/" /> : <Register />}
+            </Route>
 
             <Route path="/details" component={Details} />
             <Route path="/ask" component={PostProblem} />
