167b0589d6002349916de353fa6938ef49eeafd4
DarioTsvetanov <d.tsvetanoff@gmail.com>
2021-02-09 20:14:35 +0200

    Update topicController.js

--- server/src/controllers/topicController.js
+++ server/src/controllers/topicController.js
@@ -3,8 +3,12 @@ const router = Router();
 
 const topicService = require('../services/topicService');
 
+router.get('/', async (req, res) => {
+    console.log('In get');
+});
+
 router.post('/', async (req, res) => {
-    
+    console.log('In post');
 });
 
 module.exports = router;
