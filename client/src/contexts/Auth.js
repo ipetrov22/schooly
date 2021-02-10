09594abb54f83b4d4ed194879c366dd9f192d664
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-10 22:04:15 +0200

    AuthContext saves uid in localStorage

--- client/src/contexts/Auth.js
+++ client/src/contexts/Auth.js
@@ -4,10 +4,16 @@ import firebase from '../firebase';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const uid = localStorage.getItem('uid');
+    const [user, setUser] = useState(uid);
 
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                localStorage.setItem('uid', user.uid);
+            } else {
+                localStorage.removeItem('uid');
+            }
             setUser(user);
         });
 
