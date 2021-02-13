e683e95bbf68e62c69e2f393ccb70da89482fecb
DarioTsvetanov <d.tsvetanoff@gmail.com>
2021-02-13 16:45:53 +0200

    Changed database account

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -4,7 +4,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         PORT: process.env.PORT || 3333,
-        URI: 'mongodb+srv://Dario:userDB@schoolydb.21nfr.mongodb.net/schoolydb?retryWrites=true&w=majority',
+        URI: 'mongodb+srv://User:user1234@cluster0.lxupn.mongodb.net/<dbname>?retryWrites=true&w=majority',
     },
     production: {}
 };
