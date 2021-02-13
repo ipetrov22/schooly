aa684191bb8c7c307992de99c099c1bb0df833a9
Ivaylo Petrov <ivaylo020@gmail.com>
2021-02-13 17:40:49 +0200

    update db url

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -4,7 +4,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         PORT: process.env.PORT || 3333,
-        URI: 'mongodb+srv://Dario:userDB@schoolydb.21nfr.mongodb.net/schoolydb?retryWrites=true&w=majority',
+        URI: 'mongodb+srv://admin:admin@cluster0.lxupn.mongodb.net/Schooly?retryWrites=true&w=majority',
     },
     production: {}
 };
